292dcc9cc1578321e93d71cfdf2e6b90c8096979
Gerardo Uicab <electro.gospel277@gmail.com>
2019-02-12 00:06:57 -0600

    bien

--- DAO/PaisDAO.php
+++ DAO/PaisDAO.php
@@ -28,5 +28,41 @@ class PaisDAO
 
     }
 
+    public function modificar($objPais)
+    {
+        $con=Conexion::conectar();
+        $id=$objPais->getId();
+        $nombre=$objPais->getNombre();
+
+        $eje="update pais set NombrePais='$nombre' where id_Pais=$id";
+        if(!$con->query($eje))
+        {
+            print '<script languaje="JavaScript">alert("Error al Modificar!");</script>';
+
+        }
+        else
+        {
+            print '<script languaje="JavaScript">alert("Modificado!");</script>';
+        }
+        $con=null;
+    }
+
+    public function eliminar($objPais)
+    {
+        $con=Conexion::conectar();
+        $id=$objPais->getId();
+
+        $ejecutar="delete from pais where id_Pais=$id";
+
+        if(!$con->query($ejecutar))
+        {
+            print '<script languaje="JavaScript">alert("Error al eliminar!");</script>';
+        }
+        else
+        {
+            print '<script languaje="JavaScript">alert("Eliminado!");</script>';
+        }
+
+    }
 
 }
--- Vista/PaisAdmin.php
+++ Vista/PaisAdmin.php
@@ -1,4 +1,16 @@
-<?php include 'headerAdmin.php' ?>
+<?php include 'headerAdmin.php';
+ include '../DAO/PaisDAO.php';
+
+ $con=Conexion::conectar();
+ $eje="SELECT * FROM pais";
+ $hola=$con->query($eje);
+ $lista=$hola->fetchAll(PDO::FETCH_ASSOC);
+
+ $txtId=(isset($_POST['txtId']))?$_POST['txtId']:"";
+ $txtNombre=(isset($_POST['txtNombre']))?$_POST['txtNombre']:"";
+ 
+
+?>
  <body>
      <div class="container">
             <h1 class="heading-tittle" align="center">
@@ -29,18 +41,18 @@
                                             <div class="form-row">
                                             <div class="form-group">
                                         
-                                        <input type="hidden" class="form-control" id="txtId">
+                                        <input type="hidden" class="form-control" id="txtId" value="<?php echo $txtId; ?>" name="txtId">
                                         </div>
                                         <div class="form-group">
                                         <label for="usr">Nombre del Pais:</label>
-                                        <input type="text" class="form-control" id="txtNombre" name="txtNombre" placeholder="Nombre País">
+                                        <input type="text" class="form-control" id="txtNombre" required value="<?php echo $txtNombre; ?>" name="txtNombre" placeholder="Nombre País">
                                         </div>
                                             </div>
                                         </div>
                                         <div class="modal-footer" >
                                             <button type="submit" height="48" class="myButton" value="Agregar" name="btnAgregar">Agregar</button>
-                                            <button type="button" class="myButton">Modificar</button>
-                                            <button type="button" class="myButton">Eliminar</button>
+                                            <button type="submit" class="myButton" value="modificar" name="btnModificar">Modificar</button>
+                                            <button type="submit" class="myButton" value="eliminar" name="btnEliminar">Eliminar</button>
                                         </div>
                                     </div>
                                  </div>
@@ -56,37 +68,75 @@
     			</tr>
   			</thead>
   			<tbody>
+              <?php foreach ($lista as $pais) {?>
+                  
+              
     			<tr>
-     				 <th scope="row" hidden="true">1</th>
-     					 <td>México</td>
+     				 <th hidden="true"><?php echo $pais['id_Pais'] ?></th>
+     					 <td><?php echo $pais['NombrePais'] ?></td>
+
                           <td>
-                               <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#exampleModal">Editar</button>
-                               <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#exampleModal"><i class="">Eliminar</i></button>
+                          <form method="post" action="">
+                          <input type="hidden" name="txtId" value="<?php echo $pais['id_Pais']; ?>">
+                          <input type="hidden" name="txtNombre" value="<?php echo $pais['NombrePais']; ?>">
+                          <button type="submit" class="btn btn-success"  name="btnSeleccionar">Seleccionar</button>
+                          <button type="submit" class="btn btn-danger" name="btnEliminar">Eliminar</button>
+                          </form>
                         </td>
-   					 </tr>
+   			    </tr>
+              <?php }?>
 			</tbody>
 		</table>
    </div>
  </body>
  <?php
-    include '../DAO/PaisDAO.php';
+    
     $agre=new PaisDAO();
+    
 
     function asignar()
     {
         $pais=new PaisBO();
-
+        
+        $pais->setId($_REQUEST["txtId"]);
         $pais->setNombre($_REQUEST["txtNombre"]);
         return $pais;
 
     }
+    function obtener()
+    {
+        $pais=new PaisBO();
+        $pais->setNombre($_REQUEST["txtNombre"]);
+        return $pais;
+    }
     if(isset($_REQUEST["btnAgregar"]))
     {
-        $agre->insertar(asignar());
+        $agre->insertar(obtener());
+
+    }
+    if(isset($_REQUEST["btnModificar"]))
+    {
+        $agre->modificar(asignar());
+    }
+    if(isset($_REQUEST["btnEliminar"]))
+    {
+        $agre->eliminar(asignar());
+    }
+
+    if(isset($_REQUEST["btnSeleccionar"]))
+    {
+    
 
     }
 
-
-
 ?>
+
 <?php include 'footerAdmin.php'?>
+
+
+<script>
+
+    $('#exampleModal').modal('show');
+
+
+</script>
